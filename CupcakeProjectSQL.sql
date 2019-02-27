cb5975fab895db973ab5480fe12d18eae569fae4
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-02-27 21:04:35 +0100

    SQL

--- CupcakeProjectSQL.sql
+++ CupcakeProjectSQL.sql
@@ -1,6 +1,4 @@
-DROP SCHEMA IF EXISTS `Cupcake`;
-
-CREATE SCHEMA `Cupcake`;
+CREATE SCHEMA IF NOT EXISTS`Cupcake`;
 USE `Cupcake`;
 
 DROP TABLE IF EXISTS `Users`;
@@ -12,9 +10,9 @@ CREATE TABLE `Users`(
 PRIMARY KEY (`name`)
 );
 
-DROP TABLE IF EXISTS `buttom`;
+DROP TABLE IF EXISTS `bottom`;
 
-CREATE TABLE `buttom`(
+CREATE TABLE `bottom`(
 `bname` VARCHAR(25),
 `Price` INT(25),
 PRIMARY KEY (`bname`)
@@ -30,20 +28,22 @@ PRIMARY KEY (`tname`)
 
 DROP TABLE IF EXISTS `invoices`;
 
-CREATE TABLE `invoices`(
-`inDate` DATE DEFAULT,
-`invID` INT,
-`cName` VARCHAR(25) 
-INDEX bname_idx (bname ASC) VISIBLE,
-INDEX tname_idx (tname ASC) VISIBLE,
-	CONSTRAINT bname
-		FOREIGN KEY (bname)
-		REFERENCES Cupcake.`buttom` (bname)
-		ON DELETE CASCADE
-		ON UPDATE NO ACTION
-	CONSTRAINT tname
-		FOREIGN KEY (tname)
-		REFERENCES Cupcake.`toppings` (tname)
-		ON DELETE NO ACTION
-		ON UPDATE NO ACTION
-);
+CREATE TABLE `cupcake`.`invoices` (
+  `idinvoices` INT NOT NULL,
+  `quantity` INT NULL,
+  `bname` VARCHAR(45) NULL,
+  `tname` VARCHAR(45) NULL,
+  `date` DATETIME NOT NULL DEFAULT current_timestamp,
+  PRIMARY KEY (`idinvoices`),
+  INDEX `bname_idx` (`bname` ASC) VISIBLE,
+  INDEX `tname_idx` (`tname` ASC) VISIBLE,
+  CONSTRAINT `bname`
+    FOREIGN KEY (`bname`)
+    REFERENCES `cupcake`.`buttom` (`bname`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `tname`
+    FOREIGN KEY (`tname`)
+    REFERENCES `cupcake`.`toppings` (`tname`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
