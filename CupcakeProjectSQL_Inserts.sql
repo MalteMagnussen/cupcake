e94bc9b3c7fbbc6ee4729096448c734a0de1a7fc
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-02-28 20:05:38 +0100

    SQL insert added test user

--- CupcakeProjectSQL_Inserts.sql
+++ CupcakeProjectSQL_Inserts.sql
@@ -9,3 +9,14 @@ INSERT INTO `Cupcake`.`topping`
 `Price`)
 VALUES
 ('Chocolate', 5.00), ('Blueberry', 5.00), ('Rasberry', 5.00), ('Crispy', 6.00), ('Strawberry', 6.00), ('Rum/Raisin', 	7.00), ('Orange', 8.00), ('Lemon', 8.00), ('Blue cheese', 9.00);
+
+INSERT INTO `cupcake`.`users`
+(`name`,
+`password`,
+`balance`,
+`email`)
+VALUES
+('test',
+'test',
+0,
+'test@gmail.com');
