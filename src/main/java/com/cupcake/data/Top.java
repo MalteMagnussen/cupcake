ed62e89b7de871a15e8d7a032de0b8e8a5583128
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-02-28 10:47:29 +0100

    new classes

---
+++ src/main/java/com/cupcake/data/Top.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cupcake.data;
+
+/**
+ *
+ * @author Malte
+ */
+public class Top {
+    private String name;
+    private int price;
+
+    public Top(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+    
+}
