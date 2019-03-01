1adaa5c3caf569748bf76034387f75595b1a9cb5
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-03-01 20:44:46 +0100

    Changed ProductCommand into a RequestDispatcher

---
+++ src/main/java/com/cupcake/presentation/ProductDispatcher.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cupcake.presentation;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * TO DO
+ *
+ * @author
+ */
+public class ProductDispatcher implements RequestDispatcher {
+
+    /* 
+        Takes Requests from Shop Command. 
+        
+            - Takes query of a cupcake and adds that shit to cart.
+    
+            - Takes query of add balance and adds that to SQL.
+    
+            - Takes query of invoice and adds that to SQL and clears shoppingcart.
+    
+        Forwards back to Shop Command afterwards with new shit in session.
+        
+    */
+    
+    @Override
+    public void forward(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void include(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+ 
+
+}
