09db8db1ee2f7fd3421b45b75a6f3a48536fec41
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-03-01 13:32:39 +0100

    Add User.

--- src/main/java/com/cupcake/presentation/CreateUserCommand.java
+++ src/main/java/com/cupcake/presentation/CreateUserCommand.java
@@ -7,7 +7,9 @@ package com.cupcake.presentation;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import static java.lang.System.out;
+import java.net.URL;
 import java.nio.file.Files;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -21,37 +23,32 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class CreateUserCommand extends Command {
 
-     private static String generateHTML(String tmpl, String... args) throws Exception {
-        String file = getResourceFileContents(tmpl);
-        int con = 0;
-        for (String arg : args) {
-            file = file.replace("$" + con, arg);
-            con++;
-        }
-        return file;
-    }
-    
-    /*
-    It is not part of the curriculum (pensum) to understand this method.
-    You are more than welcome to bang your head on it though.
-     */
-    private static String getResourceFileContents(String fileName) throws Exception {
-        //Get file from resources folder
-//        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-//        URL url = classLoader.getResource(fileName);
-        //File file = new File(url.getFile());
-        File file = new File("/"+fileName);
-        String content = new String(Files.readAllBytes(file.toPath()));
-        return content;
-    }
-
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-         try {
-             out.println("" + generateHTML(getResourceFileContents("CreateUser.html")));
-         } catch (Exception ex) {
-             Logger.getLogger(CreateUserCommand.class.getName()).log(Level.SEVERE, null, ex);
-         }
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+        out.print("<!DOCTYPE html>\n"
+                + "<!--\n"
+                + "To change this license header, choose License Headers in Project Properties.\n"
+                + "To change this template file, choose Tools | Templates\n"
+                + "and open the template in the editor.\n"
+                + "-->\n"
+                + "<html>\n"
+                + "    <head>\n"
+                + "        <title>Create User</title>\n"
+                + "        <meta charset=\"UTF-8\">\n"
+                + "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+                + "    </head>\n"
+                + "    <body>\n"
+                + "        <form action=\"CreateUser\">\n"
+                + "            Name:        <input type=\"text\" name=\"username\"/><br/>\n"
+                + "            Password:    <input type=\"text\" name=\"password\"/><br/>\n"
+                + "            Email:       <input type=\"text\" name=\"email\"/><br/>\n"
+                + "            <input type=\"submit\" value=\"go\"/>\n"
+                + "        </form>\n"
+                + "    </body>\n"
+                + "</html>\n"
+                + "");}
     }
-    
+
 }
