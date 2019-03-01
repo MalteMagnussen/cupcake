dc0667367b01a1671d584c2b6313afe6daca09d7
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-03-01 13:18:59 +0100

    Add User.

--- src/main/java/com/cupcake/presentation/CreateUserCommand.java
+++ src/main/java/com/cupcake/presentation/CreateUserCommand.java
@@ -5,7 +5,12 @@
  */
 package com.cupcake.presentation;
 
+import java.io.File;
 import java.io.IOException;
+import static java.lang.System.out;
+import java.nio.file.Files;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,11 +21,37 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class CreateUserCommand extends Command {
 
+     private static String generateHTML(String tmpl, String... args) throws Exception {
+        String file = getResourceFileContents(tmpl);
+        int con = 0;
+        for (String arg : args) {
+            file = file.replace("$" + con, arg);
+            con++;
+        }
+        return file;
+    }
     
+    /*
+    It is not part of the curriculum (pensum) to understand this method.
+    You are more than welcome to bang your head on it though.
+     */
+    private static String getResourceFileContents(String fileName) throws Exception {
+        //Get file from resources folder
+//        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+//        URL url = classLoader.getResource(fileName);
+        //File file = new File(url.getFile());
+        File file = new File("/"+fileName);
+        String content = new String(Files.readAllBytes(file.toPath()));
+        return content;
+    }
 
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
+         try {
+             out.println("" + generateHTML(getResourceFileContents("CreateUser.html")));
+         } catch (Exception ex) {
+             Logger.getLogger(CreateUserCommand.class.getName()).log(Level.SEVERE, null, ex);
+         }
     }
     
 }
