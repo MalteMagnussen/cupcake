65b8f896cc828c7ce50902399b6b1d9d73fe83be
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-03-03 11:07:19 +0100

    Shop and Product

--- src/main/java/com/cupcake/presentation/ShopCommand.java
+++ src/main/java/com/cupcake/presentation/ShopCommand.java
@@ -84,58 +84,11 @@ public class ShopCommand extends Command {
             out.println("<p style=\"font-size:18px\"> "
                     + "Users Balance: " + user.getBalance() + "</p>");
 
-            /* Pulling the tops and bottoms of the cupcakes out of SQL */
-            List<Top> tops = db.getTops();
-            List<Bottom> bots = db.getBottoms();
+            // Dropdown menu:
+            dropdown(db, out);
 
-            /* Form for dropdowns BEGIN */
-            out.println(
-                    "<form id=\"addProduct\" action=\"Product\" method=\"get\">\n"
-                    + "<input type=\"hidden\" name=\"origin\" value=\"addProduct\">\n"
-                    + "<table class=\"table table-striped\">\n"
-                    + "<thead><tr><th>Bottom</th><th>Topping</th><th>Quantity</th><th>Select</th><th></th></tr></thead>\n"
-                    + "<tbody>\n"
-                    + "<tr>\n"
-                    + "<td><select name=\"bottom\" id=\"bottomSelect\">\n");
-
-            for (Bottom bot : bots) {
-                out.print("<option value=\"" + bot.getName()
-                        + "\">" + bot.getName() + "</option>\n");
-            }
-
-            out.print("<select>\n");
-            out.print("<td><select name=\"top\" id=\"topSelect\">\n");
-
-            for (Top top : tops) {
-                out.print("<option value=\"" + top.getName()
-                        + "\">" + top.getName() + "</option>\n");
-            }
-
-            out.print("</select>\n"
-                    + "<td><input type=\"text\" name=\"qty\" placeholder=\"quantity\" id=\"qtyInput\"></td>\n"
-                    + "<td><input type=\"submit\" name=\"submit\" value=\"Add to cart\"></td><td><span id=\"errorContainer\"></span></td>\n"
-                    + "</tr>\n"
-                    + "</tbody>\n"
-                    + "</table>\n"
-                    + "</form>"
-            );
-            /* Form for dropdows END */
-
-            /* Form for ShoppingCart START */
-            ShoppingCart cart = user.getCart();
-
-            out.println("<h1> ShoppingCart: </h1>");
-
-            if (cart == null) {
-
-            } else {
-                List<LineItem> items = cart.getLineItems();
-                for (LineItem item : items) {
-                    out.println("<p style=\"font-size:18px\"> "
-                            + "Cupcake: " + item.toString() + "</p>");
-                }
-            }
-            /* Form for ShoppingCart END */
+            // ShoppingCart:
+            cart(user, out);
             
             out.println("</body>");
             /* BODY END */
@@ -143,6 +96,74 @@ public class ShopCommand extends Command {
             out.println("</html>");
             /* HTML END */
         }
+    }
+
+    /**
+     * ShoppingCart.
+     * @param user
+     * @param out 
+     */
+    private void cart(User user, final PrintWriter out) {
+        /* Form for ShoppingCart START */
+        ShoppingCart cart = user.getCart();
+        
+        out.println("<h1> ShoppingCart: </h1>");
+        
+        if (cart == null) {
+            
+        } else {
+            /* Prints every LineItem in the cart in a list. */
+            List<LineItem> items = cart.getLineItems();
+            for (LineItem item : items) {
+                out.println("<p style=\"font-size:18px\"> "
+                        + "Cupcake: " + item.toString() + "</p>");
+            }
+        }
+        /* Form for ShoppingCart END */
+    }
+
+    /**
+     * Dropdown menu.
+     * @param db
+     * @param out 
+     */
+    private void dropdown(CupcakeDataMapper db, final PrintWriter out) {
+        /* Pulling the tops and bottoms of the cupcakes out of SQL */
+        List<Top> tops = db.getTops();
+        List<Bottom> bots = db.getBottoms();
+        
+        /* Form for dropdowns BEGIN */
+        out.println(
+                "<form id=\"addProduct\" action=\"Product\" method=\"get\">\n"
+                        + "<input type=\"hidden\" name=\"origin\" value=\"addProduct\">\n"
+                        + "<table class=\"table table-striped\">\n"
+                        + "<thead><tr><th>Bottom</th><th>Topping</th><th>Quantity</th><th>Select</th><th></th></tr></thead>\n"
+                        + "<tbody>\n"
+                        + "<tr>\n"
+                        + "<td><select name=\"bottom\" id=\"bottomSelect\">\n");
+        
+        for (Bottom bot : bots) {
+            out.print("<option value=\"" + bot.getName()
+                    + "\">" + bot.getName() + "</option>\n");
+        }
+        
+        out.print("<select>\n");
+        out.print("<td><select name=\"top\" id=\"topSelect\">\n");
+        
+        for (Top top : tops) {
+            out.print("<option value=\"" + top.getName()
+                    + "\">" + top.getName() + "</option>\n");
+        }
+        
+        out.print("</select>\n"
+                + "<td><input type=\"text\" name=\"qty\" placeholder=\"quantity\" id=\"qtyInput\"></td>\n"
+                + "<td><input type=\"submit\" name=\"submit\" value=\"Add to cart\"></td><td><span id=\"errorContainer\"></span></td>\n"
+                + "</tr>\n"
+                + "</tbody>\n"
+                + "</table>\n"
+                + "</form>"
+        );
+        /* Form for dropdows END */
     }
 
 }
