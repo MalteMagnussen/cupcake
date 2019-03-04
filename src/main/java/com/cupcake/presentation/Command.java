d289a6afa63084d5817eb40637f081d1f42cc834
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-03-04 15:58:24 +0100

    Improved Command a bit so it works with request GetParameter. Will have to change everything else to include that in the JSP, but otherwise we cant access the JSP. Look in teachers project.

--- src/main/java/com/cupcake/presentation/Command.java
+++ src/main/java/com/cupcake/presentation/Command.java
@@ -5,11 +5,12 @@
  */
 package com.cupcake.presentation;
 
-import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 /**
  *
  * @author Malte
@@ -23,35 +24,55 @@ public abstract class Command {
             throws ServletException, IOException;
 
     public static Command from(HttpServletRequest request) {
+        
         Command c;
-        String path = request.getPathInfo().substring(1);
-
-        /*  Switches on the paths.  */
-        switch (path) {
-            case "LoginPage":
-                c = new LoginPageCommand();
-                break;
-            case "Login":
-                c = new LoginCommand();
-                break;
-            case "Shop":
-                c = new ShopCommand();
-                break;
-            case "Product":
-                c = new ProductDispatcher();
-                break;
-            case "CreateUser":
-                c = new CreateUserCommand();
-                break;
-            case "InputUser":
-                c = new InputUserCommand();
-                break;
-            
-            /* If none of the above, then go to an error */
-            default: 
-                c = new UnknownCommand();
-        }
+        
+        String origin = request.getParameter("command");
+        
+        Map<String, Command> commands = new HashMap<>();
+                
+        commands.put("LoginPage", new LoginPageCommand());
+        commands.put("Login", new LoginCommand());
+        commands.put("Shop", new ShopCommand());
+        commands.put("Product", new ProductDispatcher());
+        commands.put("CreateUser", new CreateUserCommand());
+        commands.put("InputUser", new InputUserCommand());
+        
+        
+               
+        c = commands.getOrDefault(origin, new UnknownCommand());
+        
         return c;
+        
+//        Command c;
+//        String path = request.getPathInfo().substring(1);
+//
+//        /*  Switches on the paths.  */
+//        switch (path) {
+//            case "LoginPage":
+//                c = new LoginPageCommand();
+//                break;
+//            case "Login":
+//                c = new LoginCommand();
+//                break;
+//            case "Shop":
+//                c = new ShopCommand();
+//                break;
+//            case "Product":
+//                c = new ProductDispatcher();
+//                break;
+//            case "CreateUser":
+//                c = new CreateUserCommand();
+//                break;
+//            case "InputUser":
+//                c = new InputUserCommand();
+//                break;
+//            
+//            /* If none of the above, then go to an error */
+//            default: 
+//                c = new UnknownCommand();
+//        }
+//        return c;
     }
 
 }
---
+++ src/main/webapp/jsp/CreateUser.jsp
@@ -0,0 +1,25 @@
+<%-- 
+    Document   : CreateUser
+    Created on : Mar 4, 2019, 2:38:12 PM
+    Author     : Malte
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Create User</title>
+        <meta charset=\"UTF-8\">
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    </head>
+    <body>
+        <form method=\"post\" action=\"InputUser\">
+            <input type=\"text\" name=\"username\"/><br/>
+            <input type=\"text\" name=\"password\"/><br/>
+            <input type=\"text\" name=\"email\"/><br/>
+            <input type=\"submit\" value=\"Create User\"/>
+        </form>
+    </body>
+</html>
+
