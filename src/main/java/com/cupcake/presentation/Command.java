2361e4d63ab74a9aa57ae1b32cb45e38e0bc360e
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-03-04 19:33:50 +0100

    Fix

--- src/main/java/com/cupcake/presentation/Command.java
+++ src/main/java/com/cupcake/presentation/Command.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
 /**
  *
  * @author Malte
@@ -19,54 +20,25 @@ public abstract class Command {
 
     /*
         This is called from "FrontController"
-    */
+     */
     public abstract void execute(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException;
 
     public static Command from(HttpServletRequest request) {
-        
+
         Command c;
-        
+
         String origin = request.getParameter("command");
-        
+
         Map<String, Command> commands = new HashMap<>();
-                
+
         commands.put("Login", new LoginCommand());
         commands.put("Product", new ProductDispatcher());
-        
+
         c = commands.getOrDefault(origin, new UnknownCommand());
-        
+
         return c;
-        
-//        Command c;
-//        String path = request.getPathInfo().substring(1);
-//
-//        /*  Switches on the paths.  */
-//        switch (path) {
-//            case "LoginPage":
-//                c = new LoginPageCommand();
-//                break;
-//            case "Login":
-//                c = new LoginCommand();
-//                break;
-//            case "Shop":
-//                c = new ShopCommand();
-//                break;
-//            case "Product":
-//                c = new ProductDispatcher();
-//                break;
-//            case "CreateUser":
-//                c = new CreateUserCommand();
-//                break;
-//            case "InputUser":
-//                c = new InputUserCommand();
-//                break;
-//            
-//            /* If none of the above, then go to an error */
-//            default: 
-//                c = new UnknownCommand();
-//        }
-//        return c;
+
     }
 
 }
