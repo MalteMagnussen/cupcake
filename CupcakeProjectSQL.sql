ee43cd27c1da65b47e340e1f905abae28e06d10b
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-03-05 16:38:06 +0100

    orderdetails and invoice

--- CupcakeProjectSQL.sql
+++ CupcakeProjectSQL.sql
@@ -3,6 +3,7 @@ DROP SCHEMA IF EXISTS `Cupcake`;
 CREATE SCHEMA IF NOT EXISTS`Cupcake`;
 USE `Cupcake`;
 
+DROP TABLE IF EXISTS `Cupcake`.`ordertails`;
 DROP TABLE IF EXISTS `Cupcake`.`invoices`;
 DROP TABLE IF EXISTS `Cupcake`.`topping`;
 DROP TABLE IF EXISTS `Cupcake`.`bottom`;
@@ -28,22 +29,28 @@ CREATE TABLE `topping`(
 PRIMARY KEY (`tname`)
 );
 
-CREATE TABLE `cupcake`.`invoices` (
-  `idinvoices` INT NOT NULL,
-  `quantity` INT NULL,
-  `bname` VARCHAR(45) NULL,
-  `tname` VARCHAR(45) NULL,
-  `date` DATETIME NOT NULL DEFAULT current_timestamp,
-  PRIMARY KEY (`idinvoices`),
-  INDEX `bname_idx` (`bname` ASC) VISIBLE,
-  INDEX `tname_idx` (`tname` ASC) VISIBLE,
-  CONSTRAINT `bname`
-    FOREIGN KEY (`bname`)
-    REFERENCES `cupcake`.`bottom` (`bname`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `tname`
-    FOREIGN KEY (`tname`)
-    REFERENCES `cupcake`.`topping` (`tname`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+CREATE TABLE `invoices`(
+	`name` VARCHAR(45),
+    `id` int,
+    `date` DATETIME NOT NULL DEFAULT current_timestamp,
+    PRIMARY KEY (`id`),
+     INDEX `cname_idx` (`name` ASC) VISIBLE,
+		CONSTRAINT `name`
+			FOREIGN KEY (`name`)
+			REFERENCES `cupcake`.`users` (`name`)
+			ON DELETE NO ACTION
+			ON UPDATE NO ACTION
+);
+
+CREATE TABLE `ordertails`(
+	`id` int,
+    `bname` VARCHAR(45),
+    `tname` VARCHAR(45),
+    `qty` int,
+     INDEX `invid_idx` (`id` ASC) VISIBLE,
+		CONSTRAINT `invid`
+			FOREIGN KEY (`id`)
+			REFERENCES `cupcake`.`invoices` (`id`)
+			ON DELETE NO ACTION
+			ON UPDATE NO ACTION
+);
