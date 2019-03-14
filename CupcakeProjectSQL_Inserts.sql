d4044bd7eb53a8cebe1359fc9534b60c00c38a8c
Mikkel <44898413+Kornvalles@users.noreply.github.com>
2019-03-14 12:03:13 +0100

    Cleanup
    C -> c

--- CupcakeProjectSQL_Inserts.sql
+++ CupcakeProjectSQL_Inserts.sql
@@ -1,16 +1,16 @@
-INSERT INTO `Cupcake`.`bottom`
+INSERT INTO `cupcake`.`bottom`
 (`bname`,
 `Price`)
 VALUES
 ('Chocolate', 5.00), ('Vanilla', 5.00), ('Nutmeg', 5.00), ('Pistacio', 6.00), ('Almond', 7.00);
 
-INSERT INTO `Cupcake`.`topping`
+INSERT INTO `cupcake`.`topping`
 (`tname`,
 `Price`)
 VALUES
 ('Chocolate', 5.00), ('Blueberry', 5.00), ('Rasberry', 5.00), ('Crispy', 6.00), ('Strawberry', 6.00), ('Rum/Raisin', 7.00), ('Orange', 8.00), ('Lemon', 8.00), ('Blue cheese', 9.00);
 
-INSERT INTO `Cupcake`.`users`
+INSERT INTO `cupcake`.`users`
 (`name`,
 `password`,
 `balance`,
@@ -19,14 +19,7 @@ VALUES
 ('test',
 'test',
 0,
-'test@gmail.com');
-
-INSERT INTO `Cupcake`.`users`
-(`name`,
-`password`,
-`balance`,
-`email`)
-VALUES
+'test@gmail.com'),
 ('admin',
 '1234',
 0,
